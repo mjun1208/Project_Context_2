18cf3fbc9f2253f29d10be0102de9be76ce4d05d
Chantal <c_voort1992@hotmail.com>
2020-02-04 15:55:56 +0100

    Adding MiniMap

---
+++ Unity_Project_Context_2/Assets/MiniMap/MiniMapBehaivor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapBehaivor : MonoBehaviour
+{
+	[SerializeField]
+	private Transform CityObject;
+	[SerializeField]
+	private float f_RotateSpeed = 0.5f;
+
+	private Vector3 v_RotateStartPoint;
+
+
+	public void RotateCity() 
+	{
+		CityObject.Rotate(new Vector3(0, MouseAxis() * f_RotateSpeed * Time.deltaTime, 0));
+	}
+
+	public void RotateStart() 
+	{
+		v_RotateStartPoint = Input.mousePosition;
+	}
+
+	float MouseAxis() 
+	{
+		return v_RotateStartPoint.x - Input.mousePosition.x;
+	}
+}
