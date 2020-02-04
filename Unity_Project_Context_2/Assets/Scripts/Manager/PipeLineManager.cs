81cfe4330217e5c78ce44bc7f6f9e206881641da
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2020-02-05 01:07:21 +0900

    Place Pipe & Rotation Pipe //But Not Perfect to Place

---
+++ Unity_Project_Context_2/Assets/Scripts/Manager/PipeLineManager.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeLineManager : MonoBehaviour
+{
+    public List<GameObject> BenchPipeLine;
+    public GameObject[,] PipeLines_GameObject = new GameObject[4, 4];
+    private GameObject target_GameObject;
+    private PipeLine target_PipeLine_cs;
+
+    private bool b_IsMouseDown = false;
+    ////2.95,1,3   || 2.95,1,1  || 2.95,1,-1,  || 2.95,1,-3
+    ////0,95,1,3   || 0,95,1,1  || 0,95,1,-1,  || 0,95,1,-3
+    ////-0.95,1,3  || -0.95,1,1 || -0.95,1,-1, || -0.95,1,-3
+    ////-2,95,1,3  || -2,95,1,1 || -2,95,1,-1, || -2,95,1,-3
+
+    //
+    //            (0,3)
+    //        (0,2)   (1,3)
+    //    (0,1)   (1,2)   (2,3)
+    //(0,0)   (1,1)   (2,2)   (3,3)
+    //    (1,0)   (2,1)   (3,2)
+    //        (2,0)   (3,1)
+    //            (3,0)  
+    //
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        int i_PipeLineCount = 0;
+        
+        for (int i = 0; i < 4; i++)
+        {
+             for (int j = 0; j < 4; j++)
+             {
+                PipeLines_GameObject[i, j] = this.transform.GetChild(i_PipeLineCount).gameObject;
+                i_PipeLineCount++;
+             }
+        }
+    }
+
+    void Start()
+    {
+        //StartCoroutine(SetPipeLinePostion());
+        ResetPipeLinePosition();
+        b_IsMouseDown = false;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !b_IsMouseDown)
+        {
+            Debug.Log("Down");
+            target_GameObject = GetClickedObject();
+            if (target_GameObject != null && target_GameObject.gameObject.tag == "PipeLine")  //선택된게 나라면
+            {
+                Debug.Log("MY tag is !!! : " + target_GameObject.gameObject.tag);
+                b_IsMouseDown = true;
+                target_PipeLine_cs = target_GameObject.GetComponent<PipeLine>();
+
+                if (!target_PipeLine_cs.b_IsPlaced)
+                {
+                    target_PipeLine_cs.b_IsPlaced = true;
+                }
+                else
+                {
+                    if (target_PipeLine_cs.MyRotState == PipeLine.PipeLine_RotState.PRS_Bottom)
+                        target_PipeLine_cs.MyRotState = PipeLine.PipeLine_RotState.PRS_Left;
+                    else
+                        target_PipeLine_cs.MyRotState++;
+
+                    b_IsMouseDown = false;
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && b_IsMouseDown)
+        {
+            Debug.Log("Press");
+            Vector3 v_MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);//Input.mousePosition;
+            //v_MousePos = Camera.main.ScreenToWorldPoint(v_MousePos);
+            target_GameObject.transform.position = new Vector3(v_MousePos.x, 1.2f, v_MousePos.z);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0) && b_IsMouseDown)
+        {
+            Debug.Log("Up");
+            b_IsMouseDown = false;
+
+            target_GameObject.transform.position = SetPipeLinePosition(target_GameObject.transform.position);
+            target_GameObject = null;
+        }
+
+    }
+
+    Vector3 SetPipeLinePosition(Vector3 v_MyPosition)
+    {
+        Vector3 v_PipeLinePosition = Vector3.zero;
+        float f_MinDistance = 30f;
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                float f_CurDistance = Vector3.Distance(v_MyPosition, PipeLines_GameObject[i, j].transform.position);
+                if (f_CurDistance < f_MinDistance)
+                {
+                    f_MinDistance = f_CurDistance;
+                    v_PipeLinePosition = PipeLines_GameObject[i, j].transform.position;
+                }
+            }
+        }
+
+        return v_PipeLinePosition;
+    }
+
+    void ResetPipeLinePosition()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                PipeLines_GameObject[i, j].transform.position = new Vector3(3 - i * 2, 1.2f, 3 - j * 2);
+            }
+        }
+    }
+    //IEnumerator SetPipeLinePostion()
+    //{
+    //    while (true)
+    //    {
+    //        for (int i = 0; i < 4; i++)
+    //        {
+    //            for (int j = 0; j < 4; j++)
+    //            {
+    //                PipeLines_GameObject[i, j].transform.position = new Vector3(3 - i * 2, 1.2f , 3 - j * 2);
+    //            }
+    //        }
+    //        yield return null;
+    //    }
+    //}
+
+    private GameObject GetClickedObject()
+    {
+        RaycastHit hit;
+        target_GameObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //마우스 포인트 근처 좌표를 만든다. 
+
+        if (true == (Physics.Raycast(ray.origin, ray.direction * 10, out hit)))   //마우스 근처에 오브젝트가 있는지 확인
+        {
+            //있으면 오브젝트를 저장한다.
+            target_GameObject = hit.collider.gameObject;
+        }
+        return target_GameObject;
+    }
+}
---
+++ Unity_Project_Context_2/Assets/Scripts/PipeLine.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeLine : MonoBehaviour
+{
+    public enum PipeLine_State
+    {
+        PS_None,
+        PS_Straight,
+        PS_Corner
+    };
+    public PipeLine_State MyState;
+
+    public enum PipeLine_RotState
+    {
+        PRS_Left,
+        PRS_Top,
+        PRS_Right,
+        PRS_Bottom
+    };
+    public PipeLine_RotState MyRotState;
+
+    public bool b_IsPlaced = false;
+
+    [SerializeField] private GameObject Straight_Pipe;
+    [SerializeField] private GameObject Corner_Pipe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyState = PipeLine_State.PS_Straight;
+        MyRotState = PipeLine_RotState.PRS_Left;
+        StartCoroutine(SetState());
+        StartCoroutine(SetRotState());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SetRotState()
+    {
+        while (true)
+        {
+            switch (MyRotState)
+            {
+                case PipeLine_RotState.PRS_Left:
+                    this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                    break;
+                case PipeLine_RotState.PRS_Top:
+                    this.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                    break;
+                case PipeLine_RotState.PRS_Right:
+                    this.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                    break;
+                case PipeLine_RotState.PRS_Bottom:
+                    this.transform.rotation = Quaternion.Euler(new Vector3(0, 270, 0));
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator SetState()
+    {
+        while (true)
+        {
+            switch (MyState)
+            {
+                case PipeLine_State.PS_None:
+                    Straight_Pipe.SetActive(false);
+                    Corner_Pipe.SetActive(false);
+                    break;
+                case PipeLine_State.PS_Straight:
+                    Straight_Pipe.SetActive(true);
+                    Corner_Pipe.SetActive(false);
+                    break;
+                case PipeLine_State.PS_Corner:
+                    Straight_Pipe.SetActive(false);
+                    Corner_Pipe.SetActive(true);
+                    break;
+            }
+            yield return null;
+        }
+    }
+}
