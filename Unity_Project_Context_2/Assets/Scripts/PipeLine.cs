d4faa4990104627a0a3b60c8135a124223d5466f
mjun1208 <37032675+mjun1208@users.noreply.github.com>
2020-02-06 22:35:53 +0900

    Add PipeLine's OpenWay (Check PipeLine.cs)

--- Unity_Project_Context_2/Assets/Scripts/PipeLine.cs
+++ Unity_Project_Context_2/Assets/Scripts/PipeLine.cs
@@ -32,7 +32,8 @@ public class PipeLine : MonoBehaviour
     [SerializeField] private GameObject T_Pipe;
     [SerializeField] private GameObject X_Pipe;
 
-    [HideInInspector] public bool[] b_OpenWay = new bool[4];
+    [HideInInspector] public bool[] b_OpenWay = new bool[4];  //left top right bottom       //   top     right      
+                                                                                            //  left    bottom
 
     [HideInInspector] public Vector3 v_MyPosition;
 
@@ -43,14 +44,15 @@ public class PipeLine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(SetState());
+        StartCoroutine(SetRotState());
+        StartCoroutine(SetOpenWay());
     }
 
     public void Reset_PipeLine_Info()
     {
         MyState = (PipeLine_State)Random.Range(1, 5); //.PS_Corner;
         MyRotState = (PipeLine_RotState)Random.Range(0, 4);
-        StartCoroutine(SetState());
-        StartCoroutine(SetRotState());
     }
 
     public void Exchange_PipeLine_Info(PipeLine Target_cs)
@@ -58,8 +60,8 @@ public class PipeLine : MonoBehaviour
         MyState = Target_cs.MyState;
         MyRotState = Target_cs.MyRotState;
 
-        StartCoroutine(SetState());
-        StartCoroutine(SetRotState());
+        //StartCoroutine(SetState());
+        //StartCoroutine(SetRotState());
 
         //b_IsPlaced = true;
     }
@@ -68,8 +70,8 @@ public class PipeLine : MonoBehaviour
     {
         MyState = PipeLine_State.PS_None;
         MyRotState = (PipeLine_RotState)Random.Range(0, 4);
-        StartCoroutine(SetState());
-        StartCoroutine(SetRotState());
+        //StartCoroutine(SetState());
+        //StartCoroutine(SetRotState());
 
         b_IsPlaced = false;
 
@@ -150,6 +152,130 @@ public class PipeLine : MonoBehaviour
                     X_Pipe.SetActive(true);
 
                     boxCollider.enabled = true;
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator SetOpenWay()
+    {
+        while (true)
+        {
+            switch (MyState)
+            {
+                case PipeLine_State.PS_None:
+                    for (int i = 0; i < 4; i++)
+                        b_OpenWay[i] = false;
+                    break;
+                case PipeLine_State.PS_Corner:
+
+                    switch (MyRotState)
+                    {
+                        case PipeLine_RotState.PRS_Left:
+                            b_OpenWay[0] = false; //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = false; //bottom
+                            //top right
+                            break;
+                        case PipeLine_RotState.PRS_Top:
+                            b_OpenWay[0] = false; //left
+                            b_OpenWay[1] = false; //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = true;  //bottom
+                            //right bottom
+                            break;
+                        case PipeLine_RotState.PRS_Right:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = false; //top
+                            b_OpenWay[2] = false; //right
+                            b_OpenWay[3] = true;  //bottom
+                            //left bottom
+                            break;
+                        case PipeLine_RotState.PRS_Bottom:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = false; //right
+                            b_OpenWay[3] = false; //bottom
+                            //left top
+                            break;
+                    }
+
+                    break;
+                case PipeLine_State.PS_I:
+
+                    switch (MyRotState)
+                    {
+                        case PipeLine_RotState.PRS_Left:
+                            b_OpenWay[0] = false; //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = false; //right
+                            b_OpenWay[3] = true;  //bottom
+                            //top , bottom
+                            break;
+                        case PipeLine_RotState.PRS_Top:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = false; //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = false; //bottom
+                            //left , right
+                            break;
+                        case PipeLine_RotState.PRS_Right:
+                            b_OpenWay[0] = false; //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = false; //right
+                            b_OpenWay[3] = true;  //bottom
+                            //top , bottom
+                            break;
+                        case PipeLine_RotState.PRS_Bottom:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = false; //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = false; //bottom
+                            //left , right
+                            break;
+                    }
+
+                    break;
+                case PipeLine_State.PS_T:
+
+                    switch (MyRotState)
+                    {
+                        case PipeLine_RotState.PRS_Left:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = false; //bottom
+                            //left top right
+                            break;
+                        case PipeLine_RotState.PRS_Top:
+                            b_OpenWay[0] = false; //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = true;  //bottom
+                            //top right bottom
+                            break;
+                        case PipeLine_RotState.PRS_Right:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = false; //top
+                            b_OpenWay[2] = true;  //right
+                            b_OpenWay[3] = true;  //bottom
+                            //left bottom right
+                            break;
+                        case PipeLine_RotState.PRS_Bottom:
+                            b_OpenWay[0] = true;  //left
+                            b_OpenWay[1] = true;  //top
+                            b_OpenWay[2] = false; //right
+                            b_OpenWay[3] = true;  //bottom
+                            //top left bottom
+                            break;
+                    }
+
+                    break;
+                case PipeLine_State.PS_X:
+                    for (int i = 0; i < 4; i++)
+                        b_OpenWay[i] = true;
                     break;
             }
             yield return null;
