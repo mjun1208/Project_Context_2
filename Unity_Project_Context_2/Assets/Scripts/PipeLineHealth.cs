0ff8cfeac03784ab3021e4268a0a2c8cce8eeb73
Chantal <c_voort1992@hotmail.com>
2020-02-06 12:40:15 +0100

    ADDED: pipeline health script

---
+++ Unity_Project_Context_2/Assets/Scripts/PipeLineHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//put this class on the pipelines
+public class PipeLineHealth : MonoBehaviour
+{
+	[SerializeField]
+	private float f_minRandomLife = 2f, f_maxRandomLife = 10f;
+	private float f_randomLiveTime;
+
+    void Start()
+    {
+		f_randomLiveTime = Random.Range(f_minRandomLife, f_maxRandomLife);	
+	}
+
+	//call this funtion when a pipe is placed
+	public void StartBreaking() 
+	{
+		StartCoroutine(BreakDelay());
+	}
+
+	//break after a random time
+    IEnumerator BreakDelay() 
+	{
+		yield return new WaitForSeconds(f_randomLiveTime);
+		Break();
+	}
+
+	void Break() 
+	{
+		transform.GetComponent<PipeLine>().MyState = PipeLine.PipeLine_State.PS_None;
+		transform.GetComponent<PipeLine>().b_IsPlaced = false;
+		PipesSpawn.instance.ReturnPipeToPool(gameObject);
+	}
+}
